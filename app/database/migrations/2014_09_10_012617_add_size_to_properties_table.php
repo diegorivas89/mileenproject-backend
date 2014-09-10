17c61bd89687b8931611f07e145fea3ef0651f21
Diego Rivas <diegorivas89@gmail.com>
2014-09-09 22:33:39 -0300

    bug#407 - Agrego campos faltantes de la tabla properties

---
+++ app/database/migrations/2014_09_10_012617_add_size_to_properties_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddSizeToPropertiesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('properties', function(Blueprint $table)
+		{
+			$table->integer('size')->after('environment_id');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('properties', function(Blueprint $table)
+		{
+			$table->dropColumn('size');
+		});
+	}
+
+}
