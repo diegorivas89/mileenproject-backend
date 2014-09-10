e59c9c276b0a45595def2ac8f50c34b068192c8a
Diego Rivas <diegorivas89@gmail.com>
2014-09-10 19:57:29 -0300

    us #227 | task #361 - Casteo los valores en la respuesta antes de llamar al toArray

--- app/models/Environment.php
+++ app/models/Environment.php
@@ -3,8 +3,15 @@
 /**
 * 
 */
-class Environment extends Eloquent
+class Environment extends MileenModel
 {
+	public function getSchema()
+	{
+		return Array(
+			'id' => 'int',
+			'name' => 'string',
+		);
+	}
 }
 
 ?>
--- app/models/Property.php
+++ app/models/Property.php
@@ -3,8 +3,24 @@
 /**
 *
 */
-class Property extends Eloquent
+class Property extends MileenModel
 {
+	public function getSchema()
+	{
+		return Array(
+			'id' => 'int',
+			'title' => 'string',
+			'price' => 'int',
+			'currency' => 'string',
+			'address' => 'string',
+			'size' => 'int',
+			'covered_size' => 'int',
+			'environment_id' => 'int',
+			'publication_type_id' => 'int',
+		);
+	}
+
+	
 }
 
 ?>
