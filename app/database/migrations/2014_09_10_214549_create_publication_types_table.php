611ffd17ae77da12d1346a761bdf54d61f291969
Diego Rivas <diegorivas89@gmail.com>
2014-09-10 18:53:20 -0300

    us #227 | task #409 - Creo migracion de tabla operation_type

---
+++ app/database/migrations/2014_09_10_214549_create_publication_types_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePublicationTypesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('publication_types', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name', 50);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('publication_types');
+	}
+
+}
