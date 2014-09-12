b49ea2a79f4073cd53af45221ccfb999f74cd0c6
Diego Rivas <diegorivas89@gmail.com>
2014-09-11 22:21:55 -0300

    us #227 | task #361 - cambio el tipo del campo name de la tabla images

---
+++ app/database/migrations/2014_09_12_011829_remove_name_field_from_images_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class RemoveNameFieldFromImagesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('images', function(Blueprint $table)
+		{
+			$table->dropColumn('name');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('images', function(Blueprint $table)
+		{
+			$table->interger('name')->after('property_id');
+		});
+	}
+
+}
---
+++ app/database/migrations/2014_09_12_011842_add_name_field_to_images_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddNameFieldToImagesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('images', function(Blueprint $table)
+		{
+			$table->string('name', 255)->after('property_id');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('images', function(Blueprint $table)
+		{
+			$table->dropColumn('name');
+		});
+	}
+
+}
