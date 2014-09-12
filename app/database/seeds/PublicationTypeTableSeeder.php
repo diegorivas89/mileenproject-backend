e1d4b5537a0fe715b10f95d564c3ae1c35f178f8
Diego Rivas <diegorivas89@gmail.com>
2014-09-12 19:41:08 -0300

    us #365 | task #370 - agrego valores para el campo de video al seed de publication_type

--- app/database/seeds/PublicationTypeTableSeeder.php
+++ app/database/seeds/PublicationTypeTableSeeder.php
@@ -4,10 +4,10 @@ class PublicationTypeTableSeeder extends Seeder {
 
 		public function run()
 		{
-				DB::table('publication_types')->delete();
-				PublicationType::create(array('name' => 'Premiun', 'images' => 10, 'video' => 1));
-				PublicationType::create(array('name' => 'Básica', 'images' => 5, 'video' => 1));
-				PublicationType::create(array('name' => 'Gratuita', 'images' => 3, 'video' => 0));
+			DB::table('publication_types')->delete();
+			PublicationType::create(array('name' => 'Premiun', 'images' => 10, 'video' => 1));
+			PublicationType::create(array('name' => 'Básica', 'images' => 5, 'video' => 1));
+			PublicationType::create(array('name' => 'Gratuita', 'images' => 3, 'video' => 0));
 		}
 }
 
