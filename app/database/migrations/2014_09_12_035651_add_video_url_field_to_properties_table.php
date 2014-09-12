e12817ff1c1350fe8350c9fcd5f5e08783f6f930
Diego Rivas <diegorivas89@gmail.com>
2014-09-12 01:29:02 -0300

    us #227 | task #361 - agrego campo de url de video

---
+++ app/database/migrations/2014_09_12_035651_add_video_url_field_to_properties_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class AddVideoUrlFieldToPropertiesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('properties', function(Blueprint $table)
+		{
+			$table->string('video_url', 255)->after('age');
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('properties', function(Blueprint $table)
+		{
+			$table->dropColumn('video_url');
+		});
+	}
+
+}
