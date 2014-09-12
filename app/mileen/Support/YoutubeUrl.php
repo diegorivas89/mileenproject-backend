9efc5d52e2df71513bdf5b8fd6bdc8fff5fc7e98
Diego Rivas <diegorivas89@gmail.com>
2014-09-12 01:34:30 -0300

    us #227 | task #361 - agreg pequena clase para generar las urls de el video embebido y el thumbnail a partir de la url del video

---
+++ app/mileen/Support/YoutubeUrl.php
@@ -0,0 +1,57 @@
+<?php
+namespace Mileen\Support;
+/**
+ * video: https://www.youtube.com/watch?v=Bo-qweh7nbQ
+ * short: https://youtu.be/Bo-qweh7nbQ
+ * embed: https://www.youtube.com/embed/Bo-qweh7nbQ
+ * thumb: http://img.youtube.com/vi/Bo-qweh7nbQ/0.jpg
+ */
+class YoutubeUrl
+{
+	const THUMBNAIL_URL = 'http://img.youtube.com/vi/{video-id}/{thumb-number}.jpg';
+	const EMBED_URL = 'http://www.youtube.com/embed/{video-id}';
+
+	protected $videoUrl;
+
+	public static function create($videoUrl = '')
+	{
+		return new YoutubeUrl($videoUrl);
+	}
+
+	public function __construct($videoUrl = '')
+	{
+		$this->videoUrl = $videoUrl;
+	}
+
+	public function getVideoId()
+	{
+		$matches = Array();
+		if (preg_match('/youtube\.com\/watch\?v=([^&]+)/si', $this->videoUrl, $matches)){
+			return $matches[1];
+		}elseif (preg_match('/youtu\.be\/([^&]+)/si', $this->videoUrl, $matches)){
+			return $matches[1];
+		}else{
+			return '';
+		}
+	}
+
+	public function getUrl()
+	{
+		return $this->videoUrl;
+	}
+
+	public function getEmbedUrl()
+	{
+		return str_replace('{video-id}', $this->getVideoId(), self::EMBED_URL);
+	}
+
+	public function getThumbnailUrl($number = 0)
+	{
+		$thumb = str_replace('{video-id}', $this->getVideoId(), self::THUMBNAIL_URL);
+		$thumb = str_replace('{thumb-number}', $number, $thumb);
+
+		return $thumb;
+	}
+}
+
+?>
