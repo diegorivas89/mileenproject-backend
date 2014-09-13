a9dbb215f2ef7338782a30bf97105afad2ac5b89
MarianoMatayoshi <matayoshi.mariano@gmail.com>
2014-09-13 12:25:05 -0300

    US#246_Taks#252 - Agregado nuevos tipos de propiedad

--- app/database/seeds/PropertyTypeTableSeeder.php
+++ app/database/seeds/PropertyTypeTableSeeder.php
@@ -13,8 +13,15 @@ class PropertyTypeTableSeeder extends Seeder {
 				PropertyType::create(array('name' => 'Casa'));
 				PropertyType::create(array('name' => 'Local Comercial'));
 				PropertyType::create(array('name' => 'Duplex'));
-				PropertyType::create(array('name' => 'PH'));
-
+				PropertyType::create(array('name' => 'Quinta'));
+				PropertyType::create(array('name' => 'Terreno'));
+				PropertyType::create(array('name' => 'Consultorio'));
+				PropertyType::create(array('name' => 'Campo'));
+				PropertyType::create(array('name' => 'Galpón'));
+				PropertyType::create(array('name' => 'Hotel'));
+				PropertyType::create(array('name' => 'Edificio'));
+				PropertyType::create(array('name' => 'Otro'));
+				PropertyType::create(array('name' => 'Departamento Compartido'));
 		}
 
 }
