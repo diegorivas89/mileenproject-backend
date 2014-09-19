6aca1f770c88cff99ac9ed486ae4498552a2ea34
Diego Rivas <diegorivas89@gmail.com>
2014-09-19 18:02:49 -0300

    us #226 | task #282 - obtengo la propiedad a partir del identificador de la misma

---
+++ app/mileen/Api/PropertyService.php
@@ -0,0 +1,106 @@
+<?php
+namespace Mileen\Api;
+
+use \Mileen\Support\YoutubeUrl;
+use \Mileen\Support\VimeoUrl;
+
+/**
+* Busca una propiedad por id
+*/
+class PropertyService extends MileenApi
+{
+	protected $repository;
+
+	public function __construct(\Mileen\Properties\PropertyRepositoryInterface $repository)
+	{
+		parent::__construct();
+
+		$this->repository = $repository;
+	}
+
+	public function getRequiredParameters()
+	{
+		return Array('id');
+	}
+
+	public function execute($parameters)
+	{
+		try {
+			$this->assertParameters($parameters);
+			$property = $this->repository->find($parameters['id']);
+		} catch (\Exception $e) {
+			return $this->buildErrorResponse($e->getMessage());
+		}
+
+		$pictures = Array();
+		foreach ($property->getImages() as $image){
+			$pictures[] = $image->getUrl();
+		};
+
+		$property->pictures = $pictures;
+
+		$property->video = $this->getVideoResponse($property->video_url);
+		unset($property->video_url);
+
+		$property->environment = $property->getEnvironment();
+		unset($property->environment_id);
+
+		$property->propertyType = $property->getPropertyType();
+		unset($property->property_type_id);
+
+		$property->operationType = $property->getOperationType();
+		unset($property->operation_type_id);
+
+		$property->publicationType = $property->getPublicationType();
+		unset($property->publication_type_id);
+
+		$property->neighborhood = $property->getNeighborhood();
+		unset($property->neighborhood_id);
+
+		//"neighborhoodId": "385",
+        //"publicationTypeId": 22,
+
+		unset($property->credit_card_number);
+		unset($property->security_code);
+		unset($property->card_owner);
+		unset($property->created_at);
+		unset($property->updated_at);
+
+		return $this->buildResponse($property->toArray());
+	}
+
+	/**
+	 * Genera la respuesta del video para vimeo o youtube
+	 *
+	 * @param  string $url
+	 * @return array
+	 */
+	private function getVideoResponse($url)
+	{
+		if (YoutubeUrl::test($url)){
+			$youtube = YoutubeUrl::create($url);
+			$video = Array(
+				'url' 		=> $youtube->getUrl(),
+				'embed_url' => $youtube->getEmbedUrl(),
+				'thumbnail' => $youtube->getThumbnailUrl()
+			);
+		}elseif (VimeoUrl::test($url)){
+			$vimeo = VimeoUrl::create($url);
+			$video = Array(
+				'url' 		=> $vimeo->getUrl(),
+				'embed_url' => $vimeo->getEmbedUrl(),
+				'thumbnail' => $vimeo->getThumbnailUrl()
+			);
+		}else{
+			$video = Array(
+				'url' 		=> '',
+				'embed_url' => '',
+				'thumbnail' => ''
+			);
+		}
+
+		return $video;
+	}
+}
+
+?>
