b07477feca320e7ffd8adb0d9d62600d9b7fe382
Diego Rivas <diegorivas89@gmail.com>
2014-09-20 15:17:15 -0300

    task #258 - agrego campos al seed de usuario

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -2,14 +2,20 @@
 
 class UserTableSeeder extends Seeder {
 
-    public function run()
-    {
-        DB::table('users')->delete();
+	public function run()
+	{
+		DB::table('users')->delete();
 
-        User::create(array('name' => 'Ned Stark', 'telephone' => '42231231',
-                           'email' => 'ned.stark@gmail.com', 'password' => 'chau_cabeza'));
+		User::create(array(
+			'name' => 'Ned Stark', 
+			'telephone' => '42231231',
+			'email' => 'ned.stark@gmail.com', 
+			'password' => 'chau_cabeza',
+			'active' => 1,
+			'key' => '14a9f8c6f825091c7ca23da3bce1dfd8'
+		));
 
-    }
+	}
 
 }
 
