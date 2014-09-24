90560444d26ec25b3f1aa43303bec0a6e1eb2c90
Diego Rivas <diegorivas89@gmail.com>
2014-09-23 23:13:27 -0300

    bug #576 - si no hhay amenities se muestra un msj que lo indica

--- app/views/property/show.blade.php
+++ app/views/property/show.blade.php
@@ -1,11 +1,17 @@
 @extends('layout')
 @section('content')
+<style>
+	.row div.row{margin-bottom:12px;}
+	ul.features li{font-size:10pt;}
+	ul.features li .field{font-weight:bold;}
+</style>
 <div class="large-9 columns">
 	<div class='panel'>
 		<div class="row">
 			<div class="large-12 columns">
-				<h2 style="background: #f2f2f2;margin-top: 0px;padding: 20px;">
+				<h2 style="background: #f2f2f2;margin-top: 0px;">
 					<i class="fa fa-home"></i> {{$property->title}}
+					<!--
 					<button href="#" data-dropdown="drop1" aria-controls="drop1" aria-expanded="false" class="button  radius back " style="float:right;">
 						<i class="fa fa-cogs"></i>
 						{{Lang::get('strings.actions')}}
@@ -18,58 +24,78 @@
 								{{Lang::get('strings.edit')}}
 							</a>
 						</li>
-					</ul>
+					</ul>-->
 				</h2>
 			</div>
 		</div>
 		<div class="row">
-			<div class="large-12 columns">
-				<h5>
-					{{$property->getOperationType()->name}} |
-					{{$property->getPropertyType()->name}}  |
-					{{$property->getEnvironment()->name}} |
-					{{$property->getNeighborhood()->name}} |
-					{{$property->size}} m2 |
-					{{$property->age}} años
-				</h5>
+			<div class="large-6 medium-6 columns">
+				<ul class="features">
+					<li><span class="field">Tipo de Operación:</span> {{$property->getOperationType()->name}}</li>
+					<li><span class="field">Tipo de Propiedad:</span> {{$property->getPropertyType()->name}}</li>
+					<li><span class="field">Tipo de Publicación:</span> {{$property->getPublicationType()->name}}</li>
+					<li><span class="field">Ambientes:</span> {{$property->getEnvironment()->name}}</li>
+					<li><span class="field">Barrio:</span> {{$property->getNeighborhood()->name}}</li>
+				</ul>
+			</div>
+			<div class="large-6 medium-6 columns">
+				<ul class="features">
+					<li><span class="field">Superficie:</span> {{$property->size}} m2</li>
+					<li><span class="field">Superficie cubierta:</span> {{$property->covered_size}} m2</li>
+					<li><span class="field">Antigüedad:</span> {{$property->age}} años</li>
+					<li><span class="field">Precio:</span> {{$property->currency}}{{$property->price}}</li>
+					<li><span class="field">Expensas:</span> {{$property->currency}} {{$property->expenses}}</li>
+				</ul>
 			</div>
 		</div>
-		<div class="content active" id="panel1">
-			<div class="row">
-				<div class="large-12 columns">
-					<ul class="example-orbit" data-orbit>
-						@foreach($images as $image)
-							<li class="">
-								<img src="{{$image->getUrl()}}" alt="slide 2" class='parent-width carousel-height'/>
-							</li>
-						@endforeach
-						@if (!empty($property->video_url))
-							<li>
-								<div class="flex-video">
-									<iframe width="300" src="{{$video['embed_url']}}" frameborder="0" allowfullscreen></iframe>
-								</div>
-							</li>
-						@endif
-					</ul>
+		
+			<div class="content active" id="panel1">
+				<div class="row">
+					<div class="large-12 columns">
+						<ul class="example-orbit" data-orbit>
+							@foreach($images as $image)
+								<li class="">
+									<img src="{{$image->getUrl()}}" alt="slide 2" class='parent-width carousel-height'/>
+								</li>
+							@endforeach
+							@if (!empty($property->video_url))
+								<li>
+									<div class="flex-video">
+										<iframe width="300" src="{{$video['embed_url']}}" frameborder="0" allowfullscreen></iframe>
+									</div>
+								</li>
+							@endif
+						</ul>
+					</div>
 				</div>
 			</div>
-		</div>
+		
 		<div class="row">
 			<div class="columns large-12">
 				<h3>{{Lang::get('strings.description')}}</h3>
-				{{$property->description}}
+				@if (strlen(trim($property->description)) > 0)
+					{{nl2br($property->description)}}
+				@else
+					<h4><small><i>Esta publicación no posee descripción.</i></small></h4>
+				@endif
 			</div>
 		</div>
 		<div class="row">
 			<div class="columns large-12">
 				<h4>{{Lang::get('strings.amenities')}}</h4>
 				<div class="row">
-					@foreach($amenities as $amenity)
-						<div class="columns small-12 medium-6 large-4 end">
-							<i class="fa fa-check"></i>
-							<span> {{$amenity}} </span>
+					@if (count($amenities) > 0)
+						@foreach($amenities as $amenity)
+							<div class="columns small-12 medium-6 large-4 end">
+								<i class="fa fa-check"></i>
+								<span> {{$amenity}} </span>
+							</div>
+						@endforeach
+					@else
+						<div class="columns large-12">
+							<h4><small><i>Esta publicación no posee amenities.</i></small></h4>
 						</div>
-					@endforeach
+					@endif
 				</div>
 			</div>
 		</div>
