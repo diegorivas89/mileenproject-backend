0a0002c2ad3d81d9cd56a2ff29b83f37bdd1eb12
Diego Rivas <diegorivas89@gmail.com>
2014-10-14 17:15:04 -0300

    task #348 - seteo ruta al boton de cancel y agrego formulario

--- app/views/profile/edit.blade.php
+++ app/views/profile/edit.blade.php
@@ -7,33 +7,35 @@
 			<h2><i class="fa fa-user"></i> Mi Perfil</h2>
 		</div>
 	</div>
-    <div class='panel'>
-        <div class='row'>
-            <div class='small-12 medium-6 columns end'>
-                <span>Nombre</span>
-                <input type='text' value='{{$user->name}}' disabled>
-            </div>
-            <div class='small-12 medium-6 columns end'>
-                <span>Email</span>
-                <input type='email' value='{{$user->email}}' disabled>
-            </div>
-        </div>
-        <div class='row'>
-            <div class='small-12 medium-6 columns end'>
-                <span>Teléfono</span>
-                <input type='text' value='{{$user->telephone}}' disabled>
-            </div>
-            <div class='small-12 medium-6 columns end'>
-                <span>Contraseña</span>
-                <input type='password' value='' disabled>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-    	<div class="columns small-12">
-    		<input type="submit" value="Guardar" class="button" id="submit-property">
-			<a href="#" class="button secondary">Cancelar</a>
-    	</div>
-    </div>
+	<form action="" method="post">
+		<div class='panel'>
+	        <div class='row'>
+	            <div class='small-12 medium-6 columns end'>
+	                <span>Nombre</span>
+	                <input type='text' value='{{$user->name}}' disabled>
+	            </div>
+	            <div class='small-12 medium-6 columns end'>
+	                <span>Email</span>
+	                <input type='email' value='{{$user->email}}' disabled>
+	            </div>
+	        </div>
+	        <div class='row'>
+	            <div class='small-12 medium-6 columns end'>
+	                <span>Teléfono</span>
+	                <input type='text' value='{{$user->telephone}}' disabled>
+	            </div>
+	            <div class='small-12 medium-6 columns end'>
+	                <span>Contraseña</span>
+	                <input type='password' value='' disabled>
+	            </div>
+	        </div>
+	    </div>
+	    <div class="row">
+	    	<div class="columns small-12">
+	    		<input type="submit" value="Guardar" class="button" id="submit-property">
+				<a href="{{URL::action('properties.index')}}" class="button secondary">Cancelar</a>
+	    	</div>
+	    </div>
+	</form>
 </div>
 @stop
