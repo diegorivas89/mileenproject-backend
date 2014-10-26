cd256bd90943b0a65965c2c74602b019e9d694ea
Diego Rivas <diegorivas89@gmail.com>
2014-10-25 21:43:51 -0300

    task #658 - agrego seeder con algunas zonas aledañas

---
+++ app/database/seeds/AdjacentsTableSeeder.php
@@ -0,0 +1,138 @@
+<?php
+
+class AdjacentsTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('adjacents')->delete();
+
+        $index = ['Saavedra',
+'Nuñez',
+'Coghlan',
+'Belgrano',
+'Colegiales',
+'Palermo',
+'Recoleta',
+'Retiro',
+'Puerto Madero',
+'San Nicolas',
+'Montserrat',
+'San Telmo',
+'Boca',
+'Constitucion',
+'San Cristobal',
+'Parque Patricios',
+'Barracas',
+'Balvanera',
+'Boedo',
+'Almagro',
+'Villa Crespo',
+'Caballito',
+'Parque Chacabuco',
+'Nueva Pompeya',
+'Villa Soldati',
+'Flores',
+'Villa Mitre',
+'La Paternal',
+'Chacarita',
+'Villa Ortuzar',
+'Villa Urquiza',
+'Villa Pueyrredon',
+'Agronomia',
+'Villa del Parque',
+'Villa Santa Rita',
+'Floresta',
+'Velez Sarsfield',
+'Villa Luro',
+'Monte Castro',
+'Villa Devoto',
+'Villa Real',
+'Versalles',
+'Liniers',
+'Mataderos',
+'Villa Lugano',
+'Parque Avellaneda',
+'Villa Lugano',
+'Villa Riachuelo',];
+        sort($index);
+
+        $aux = [];
+        $i = 1;
+        foreach ($index as $value) {
+            $aux[$value] = $i;
+            $i++;
+        }
+        $index = $aux;
+
+        $nighborhoodsAdjacents = ['Saavedra'            => ['Nuñez', 'Coghlan', 'Villa Urquiza'],
+'Nuñez'             => ['Saavedra', 'Coghlan', 'Belgrano'],
+'Coghlan'           => ['Nuñez', 'Saavedra', 'Belgrano', 'Villa Urquiza'],
+'Belgrano'          => ['Nuñez', 'Coghlan', 'Villa Urquiza', 'Villa Ortuzar', 'Colegiales', 'Palermo'],
+'Colegiales'        => ['Belgrano', 'Villa Ortuzar', 'Chacarita', 'Palermo'],
+'Palermo'           => ['Belgrano', 'Colegiales', 'Villa Crespo', 'Almagro', 'Recoleta'],
+'Recoleta'          => ['Palermo', 'Almagro', 'Balvanera', 'San Nicolas', 'Retiro', 'Puerto Madero'],
+'Retiro'            => ['Recoleta', 'Puerto Madero', 'San Nicolas'],
+'Puerto Madero'     => ['Recoleta', 'Retiro', 'San Nicolas', 'Montserrat', 'San Telmo', 'Boca'],
+'San Nicolas'       => ['Recoleta', 'Retiro', 'Balvanera', 'Montserrat', 'Puerto Madero', '', '', '', ],
+'Montserrat'        => ['San Nicolas', 'Puerto Madero', 'San Telmo', 'Constitucion', 'Balvanera', '', '', '', ],
+'San Telmo'         => ['Montserrat', 'Constitucion', 'Barracas', 'Boca', 'Puerto Madero', '', '', '', ],
+'Boca'              => ['Puerto Madero', 'Barracas', 'San Telmo', '', '', '', '', '', ],
+'Constitucion'      => ['', '', 'Balvanera', 'San Cristobal', 'Parque Patricios', 'Barracas', 'San Telmo', 'Montserrat', ],
+'San Cristobal'     => ['', '', 'Almagro', 'Boedo', 'Parque Patricios', 'Constitucion', 'Montserrat', 'Balvanera', ],
+'Parque Patricios'  => ['', '', '', 'Barracas', 'Constitucion', 'San Cristobal', 'Boedo', 'Nueva Pompeya', ],
+'Barracas'          => ['', '', '', 'Nueva Pompeya', 'Parque Patricios', 'Constitucion', 'San Telmo', 'Boca', ],
+'Balvanera'         => ['', 'San Nicolas', 'Montserrat', 'Constitucion', 'San Cristobal', 'Boedo', 'Almagro', 'Recoleta', ],
+'Boedo'             => ['', 'Balvanera', 'San Cristobal', 'Parque Patricios', 'Nueva Pompeya', 'Parque Chacabuco', 'Caballito', 'Almagro', ],
+'Almagro'           => ['', 'Recoleta', 'Balvanera', 'San Cristobal', 'Boedo', 'Caballito', 'Villa Crespo', 'Palermo', ],
+'Villa Crespo'      => ['', '', '', '', '', '', '', '', ],
+'Caballito'         => ['', '', '', '', '', '', '', '', ],
+'Parque Chacabuco'  => ['', '', '', '', '', '', '', '', ],
+'Nueva Pompeya'     => ['', '', '', '', '', '', '', '', ],
+'Villa Soldati'     => ['', '', '', '', '', '', '', '', ],
+'Flores'            => ['', '', '', '', '', '', '', '', ],
+'Villa Mitre'       => ['', '', '', '', '', '', '', '', ],
+'La Paternal'       => ['', '', '', '', '', '', '', '', ],
+'Chacarita'         => ['', '', '', '', '', '', '', '', ],
+'Villa Ortuzar'     => ['', '', '', '', '', '', '', '', ],
+'Villa Urquiza'     => ['', '', '', '', '', '', '', '', ],
+'Villa Pueyrredon'  => ['', '', '', '', '', '', '', '', ],
+'Agronomia'         => ['', '', '', '', '', '', '', '', ],
+'Villa del Parque'  => ['', '', '', '', '', '', '', '', ],
+'Villa Santa Rita'  => ['', '', '', '', '', '', '', '', ],
+'Floresta'          => ['', '', '', '', '', '', '', '', ],
+'Velez Sarsfield'   => ['', '', '', '', '', '', '', '', ],
+'Villa Luro'        => ['', '', '', '', '', '', '', '', ],
+'Monte Castro'      => ['', '', '', '', '', '', '', '', ],
+'Villa Devoto'      => ['', '', '', '', '', '', '', '', ],
+'Villa Real'        => ['', '', '', '', '', '', '', '', ],
+'Versalles'         => ['', '', '', '', '', '', '', '', ],
+'Liniers'           => ['', '', '', '', '', '', '', '', ],
+'Mataderos'         => ['', '', '', '', '', '', '', '', ],
+'Villa Lugano'      => ['', '', '', '', '', '', '', '', ],
+'Parque Avellaneda' => ['', '', '', '', '', '', '', '', ],
+'Villa Lugano'      => ['', '', '', '', '', '', '', '', ],
+'Villa Riachuelo'   => ['', '', '', '', '', '', '', '', ],];
+
+        foreach ($nighborhoodsAdjacents as $neighborhood => $adjacents){
+            if (!$neighborhood) continue;
+            $id = $index[$neighborhood];
+            foreach ($adjacents as $adjacent) {
+                if (!$adjacent) continue;
+                $adjacentId = $index[$adjacent];
+                //echo "$id - $adjacentId\n";
+                $a = new Adjacent();
+                $a->neighborhood_id = $id;
+                $a->adjacent_neighborhood_id = $adjacentId;
+                $a->save();
+                /*
+                Adjacent::create(array(
+                        'neighborhood_id' => $id,
+                        'adjacent_neighborhood_id' => $adjacentId
+                    )
+                );*/
+            }
+        }
+    }
+}
+
+?>
--- app/models/Neighborhood.php
+++ app/models/Neighborhood.php
@@ -12,6 +12,41 @@ class Neighborhood extends MileenModel
 			'name' => 'string',
 		);
 	}
+
+	/**
+	 * Retorna los barrios adyacentes
+	 *
+	 * @return \Illuminate\Support\Collection
+	 */
+	public function getAdjacents()
+	{
+		$adjacents = Adjacents::select('adjacent_neighborhood_id')->where('neighborhood_id', '=', $this->id)->get();
+
+		$collection = new \Illuminate\Support\Collection();
+
+		foreach ($adjacents as $adjacent) {
+			$collection->add(self::find($adjacent));
+		}
+
+		return $collection;
+	}
+
+	/**
+	 * Retorna el precio por metro cuadrado para el barrio, no tiene en cuenta la moneda
+	 *
+	 * @return int
+	 */
+	public function priceByM2()
+	{
+		$properties = Property::where('neighborhood_id', '=', $this->id)->get();
+
+		$acum = 0;
+		foreach ($properties as $property) {
+			$acum += $property->price / $property->size;
+		}
+
+		return round($acum / $properties->count());
+	}
 }
 
 ?>
