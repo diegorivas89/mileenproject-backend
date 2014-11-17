5d725747c6360d096d78205508893c7c572d53c8
Diego Rivas <diegorivas89@gmail.com>
2014-11-17 18:52:51 -0300

    bug #962 - reconozco el entorno a traves del parametro

--- app/models/Ad.php
+++ app/models/Ad.php
@@ -25,12 +25,13 @@ class Ad extends MileenModel
 
 	public static function getAd()
 	{
-		if (Config::get('ads.random')){
+		//if (Config::get('ads.random')){
+		if (Input::has('mode') && Input::get('mode') == 'fixed'){
+			return new Ad(Config::get('ads.default_ad'));
+		}else{
 			$fields = ['id', 'title', 'description', 'target_url', 'banner'];
 			//retornar un aviso de la db random
 			return Ad::orderByRaw("RAND()")->select($fields)->first();
-		}else{
-			return new Ad(Config::get('ads.default_ad'));
 		}
 	}
 }
